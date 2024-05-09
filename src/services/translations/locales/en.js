6075f5f2b55709986020edbe5592ceddce0da647
Yakub Jorayev <jorayev2494@gmail.com>
2024-05-10 03:46:18 +0400

    fixed search locales

--- src/services/translations/locales/en.js
+++ src/services/translations/locales/en.js
@@ -2,7 +2,7 @@ import system from './system/en.json'
 import navigation from './navigation/en.json'
 import pages from './pages/en.json'
 import applicationForm from './applicationForm/en.json'
-import search from '@/views/pages/website/public/search/locales/en.json'
+import search from '../../../views/pages/website/public/university/pages/search/locales/en.json'
 import companyContext from './companyContext/en.js'
 import department from '../../../views/pages/website/public/department/locales/en.json'
 
