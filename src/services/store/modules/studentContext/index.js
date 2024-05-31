cd2210e527c576266a44fc3a586786cdbed11ff9
Yakub Jorayev <jorayev2494@gmail.com>
2024-05-31 20:35:45 +0400

    Fixed student employers

--- src/services/store/modules/studentContext/index.js
+++ src/services/store/modules/studentContext/index.js
@@ -1,10 +1,7 @@
 // import company from './company/index.js'
 import auth from '@/views/pages/website/studentContext/auth/store/index.js'
-import student from '@/views/pages/website/studentContext/student/store/index.js'
 import profile from '@/views/pages/website/studentContext/profile/store/index.js'
 import application from '@/views/pages/website/studentContext/application/store/index.js'
-import language  from '@/views/pages/website/studentContext/language/store/index.js'
-import employee  from '@/views/pages/website/studentContext/employee/store/index.js'
 import department  from '@/views/pages/website/studentContext/department/store/index.js'
 import announcement  from '@/views/pages/website/studentContext/announcement/store/index.js'
 import contest  from '@/views/pages/website/studentContext/contest/store/index.js'
@@ -14,11 +11,8 @@ export default {
   modules: {
     // company,
     auth,
-    student,
     profile,
     application,
-    language,
-    employee,
     department,
     announcement,
     contest,
